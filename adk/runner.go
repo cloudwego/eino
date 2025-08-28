745ce6ea2f1565d54e632e8283b21a7524dc09f2
Megumin <wangdezheng@bytedance.com>
2025-08-28 20:02:41 +0800

    feat(adk): support WithSessionValues agent run option (#428)

--- adk/runner.go
+++ adk/runner.go
@@ -60,6 +60,8 @@ func (r *Runner) Run(ctx context.Context, messages []Message,
 
 	ctx = ctxWithNewRunCtx(ctx)
 
+	SetSessionValues(ctx, o.sessionValues)
+
 	iter := fa.Run(ctx, input, opts...)
 	if r.store == nil {
 		return iter
