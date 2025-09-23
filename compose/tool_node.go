e1e3bef9e26ab3d8e4d89d10ea257845035550db
shentong.martin <shentong.martin@bytedance.com>
2025-09-23 10:12:31 +0800

    refactor: define GetInterruptState and GetResumeContext utility functions
    Change-Id: I2e6357acec95433e648fbdc8509ef44e6eb22dce

--- compose/tool_node.go
+++ compose/tool_node.go
@@ -433,14 +433,9 @@ func (tn *ToolsNode) Invoke(ctx context.Context, input *schema.Message,
 		}
 	}
 
-	if input == nil {
-		rCtx, exist := GetRunCtx(ctx)
-		if exist {
-			tnState, ok := rCtx.InterruptData.State.(*toolsInterruptAndRerunState)
-			if ok {
-				input = tnState.Input
-			}
-		}
+	tnState, hasState, interrupted := GetInterruptState[*toolsInterruptAndRerunState](ctx)
+	if interrupted && hasState {
+		input = tnState.Input
 	}
 
 	tasks, err := tn.genToolCallTasks(ctx, tuple, input, opt.executedTools, false)
@@ -515,14 +510,9 @@ func (tn *ToolsNode) Stream(ctx context.Context, input *schema.Message,
 		}
 	}
 
-	if input == nil {
-		rCtx, exist := GetRunCtx(ctx)
-		if exist {
-			tnState, ok := rCtx.InterruptData.State.(*toolsInterruptAndRerunState)
-			if ok {
-				input = tnState.Input
-			}
-		}
+	tnState, hasState, interrupted := GetInterruptState[*toolsInterruptAndRerunState](ctx)
+	if interrupted && hasState {
+		input = tnState.Input
 	}
 
 	tasks, err := tn.genToolCallTasks(ctx, tuple, input, opt.executedTools, true)
