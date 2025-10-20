88f1b285999b693f3e4a6eb795020f1da7ecfb46
Megumin <wangdezheng@bytedance.com>
2025-10-20 18:01:45 +0800

    chore

--- adk/agent_tool.go
+++ adk/agent_tool.go
@@ -57,7 +57,7 @@ func WithAgentInputSchema(schema *schema.ParamsOneOf) AgentToolOption {
 	}
 }
 
-func NewAgentTool(_ context.Context, agent Agent, options ...AgentToolOption) tool.InvokableTool {
+func NewAgentTool(_ context.Context, agent Agent, options ...AgentToolOption) tool.BaseTool {
 	opts := &AgentToolOptions{}
 	for _, opt := range options {
 		opt(opts)
