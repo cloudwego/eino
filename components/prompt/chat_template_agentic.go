48ea89c6e84815cc094f8dcf2ac0bef5a2a87ac2
Megumin <wangdezheng@bytedance.com>
2025-11-26 21:11:48 +0800

    chore

--- components/prompt/chat_template_agentic.go
+++ components/prompt/chat_template_agentic.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 CloudWeGo Authors
+ * Copyright 2025 CloudWeGo Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,3 @@
  */
 
 package prompt
-
-import (
-	"context"
-
-	"github.com/cloudwego/eino/schema"
-)
-
-var _ ChatTemplate = &DefaultChatTemplate{}
-
-type ChatTemplate interface {
-	Format(ctx context.Context, vs map[string]any, opts ...Option) ([]*schema.Message, error)
-}
-
-type AgenticChatTemplate interface {
-	Format(ctx context.Context, vs map[string]any, opts ...Option) ([]*schema.AgenticMessage, error)
-}
