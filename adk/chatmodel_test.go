465f35d8ce326151c1d765cd055255b26ef2efc1
shentongmartin <shentong.martin@bytedance.com>
2026-02-02 17:14:57 +0800

    fix(adk): change ReturnDirectly type from map[string]struct{} to map[string]bool (#737)
    - Update ToolsConfig.ReturnDirectly type
    - Update ChatModelAgentContext.ReturnDirectly type
    - Update internal types: chatModelAgentExecCtx, execContext, reactConfig
    - Update all related function signatures
    - Update test files with new type syntax
    
    Change-Id: I7d819f1c44da91b76cf9a9f867a88008068153b8

--- adk/chatmodel_test.go
+++ adk/chatmodel_test.go
@@ -655,8 +655,8 @@ func TestParallelReturnDirectlyToolCall(t *testing.T) {
 					&myTool{name: "tool3", desc: "tool3", waitTime: 100 * time.Millisecond},
 				},
 			},
-			ReturnDirectly: map[string]struct{}{
-				"tool1": {},
+			ReturnDirectly: map[string]bool{
+				"tool1": true,
 			},
 		},
 	})
