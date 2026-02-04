a312cd28cb78aba7234e95b40d4902efc0e69862
Megumin <wangdezheng@bytedance.com>
2026-02-04 14:30:10 +0800

    feat(adk): support chinese (#730)

--- adk/middlewares/filesystem/large_tool_result.go
+++ adk/middlewares/filesystem/large_tool_result.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/slongfield/pyfmt"
 
+	"github.com/cloudwego/eino/adk/internal"
 	"github.com/cloudwego/eino/compose"
 	"github.com/cloudwego/eino/schema"
 )
@@ -106,7 +107,14 @@ func (t *toolResultOffloading) handleResult(ctx context.Context, result string, 
 		}
 
 		nResult := formatToolMessage(result)
-		nResult, err = pyfmt.Fmt(tooLargeToolMessage, map[string]any{
+		msgTemplate, err := internal.SelectPrompt(internal.I18nPrompts{
+			English: tooLargeToolMessage,
+			Chinese: tooLargeToolMessageChinese,
+		})
+		if err != nil {
+			return "", err
+		}
+		nResult, err = pyfmt.Fmt(msgTemplate, map[string]any{
 			"tool_call_id":   input.CallID,
 			"file_path":      path,
 			"content_sample": nResult,
