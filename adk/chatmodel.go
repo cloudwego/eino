e277aa4bca5c40baa22bf7be2ac9e52498d5cd2a
shentong.martin <shentong.martin@bytedance.com>
2026-03-23 10:39:45 +0800

    fix: rebase error
    Change-Id: If20fa78dba82a1c177c8ec47090050ea8c1354ed

--- adk/chatmodel.go
+++ adk/chatmodel.go
@@ -1204,7 +1204,7 @@ func (a *ChatModelAgent) Resume(ctx context.Context, info *ResumeInfo, opts ...A
 			generator.Send(&AgentEvent{Err: err})
 			generator.Close()
 		}()
-		return iterator
+		return iterator, notCancellableFuncInternal
 	}
 
 	var historyModifier func(ctx context.Context, history []Message) []Message
