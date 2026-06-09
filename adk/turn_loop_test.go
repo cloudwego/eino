d658394463a5e7f39a0face97ef633cf3323c0df
shentong.martin <shentong.martin@bytedance.com>
2026-06-09 17:29:33 +0800

    feat(adk): support session event id generator
    Change-Id: I29e8e458608bec90a4b5a779c4299fa7d1ce5d65

--- adk/turn_loop_test.go
+++ adk/turn_loop_test.go
@@ -2685,7 +2685,7 @@ func TestTurnLoop_ManagedInterrupt_StartNewTurnUsesConfiguredSessionService(t *t
 		InterruptMode:  TurnLoopInterruptWaitsForExplicitResume,
 		SessionID:      sessionID,
 		SessionService: sessionStore,
-		SessionConfig:  &SessionConfig{EventFlushBatchSize: 1},
+		SessionConfig:  &SessionConfig[*schema.Message]{EventFlushBatchSize: 1},
 		GenInput:       genInputConsumeAllWithMsg,
 		GenResume: func(ctx context.Context, _ *TurnLoop[string, *schema.Message], interruptedItems, unhandledItems, resumeItems []string) (*GenResumeResult[string, *schema.Message], error) {
 			return &GenResumeResult[string, *schema.Message]{
@@ -2759,7 +2759,7 @@ func TestTurnLoop_ManagedInterrupt_DecisionResumeUsesCapturedCheckpointIDAndPara
 		InterruptMode:  TurnLoopInterruptWaitsForExplicitResume,
 		SessionID:      sessionID,
 		SessionService: sessionStore,
-		SessionConfig:  &SessionConfig{EventFlushBatchSize: 1},
+		SessionConfig:  &SessionConfig[*schema.Message]{EventFlushBatchSize: 1},
 		GenInput:       genInputConsumeAllWithMsg,
 		GenResume: func(ctx context.Context, _ *TurnLoop[string, *schema.Message], interruptedItems, unhandledItems, resumeItems []string) (*GenResumeResult[string, *schema.Message], error) {
 			require.NotEmpty(t, interruptTargetID)
@@ -4128,7 +4128,7 @@ func TestTurnLoop_PassesSessionFencingTokenToInternalRunner(t *testing.T) {
 			atomic.AddInt32(&tokenCalls, 1)
 			return "token-1", nil
 		},
-		SessionConfig: &SessionConfig{PersistenceMode: SessionPersistenceModeSync},
+		SessionConfig: &SessionConfig[*schema.Message]{PersistenceMode: SessionPersistenceModeSync},
 	})
 	loop.Run(ctx)
 	ok, _ := loop.Push("work")
