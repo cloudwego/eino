2e472aac815128f11280161055c23cb788915a11
shentong.martin <shentong.martin@bytedance.com>
2026-06-11 12:38:12 +0800

    test(adk): stabilize turn loop cancel mock
    Change-Id: Ie4c567b307f834ad9e3e2a7efadaaab06623bfb8

--- adk/turn_loop_test.go
+++ adk/turn_loop_test.go
@@ -101,8 +101,6 @@ type turnLoopCancellableMockAgent struct {
 	name     string
 	runFunc  func(ctx context.Context, input *AgentInput) (*AgentOutput, error)
 	onCancel func(cc *cancelContext)
-	cancel   context.CancelFunc
-	mu       sync.Mutex
 }
 
 func (a *turnLoopCancellableMockAgent) Name(_ context.Context) string        { return a.name }
@@ -114,10 +112,8 @@ func (a *turnLoopCancellableMockAgent) Run(ctx context.Context, input *AgentInpu
 	o := getCommonOptions(nil, opts...)
 	cc := o.cancelCtx
 
-	a.mu.Lock()
 	var cancelCtx context.Context
-	cancelCtx, a.cancel = context.WithCancel(ctx)
-	a.mu.Unlock()
+	cancelCtx, cancel := context.WithCancel(ctx)
 
 	go func() {
 		defer gen.Close()
@@ -131,11 +127,7 @@ func (a *turnLoopCancellableMockAgent) Run(ctx context.Context, input *AgentInpu
 				if a.onCancel != nil {
 					a.onCancel(cc)
 				}
-				a.mu.Lock()
-				if a.cancel != nil {
-					a.cancel()
-				}
-				a.mu.Unlock()
+				cancel()
 			}()
 		}
 
